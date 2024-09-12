1988284b61bd10cfac9a674add2e408d563703a6
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-09-12 15:17:48 +0000

    HtmlContext component

---
+++ components/HtmlContext.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HtmlContentProps {
+  content: string;
+}
+
+const HtmlContent: React.FC<HtmlContentProps> = ({ content }) => {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  );
+};
+
+export default HtmlContent;
